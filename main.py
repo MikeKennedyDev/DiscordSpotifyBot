e92db5fd0ef48d1a747f9615bcb48f8cdadc7831
unknown <mike.kennedy.dev@gmail.com>
2022-09-23 17:53:15 -0400

    Working playlist add

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
+
 import discord
 import GlobalSettings
 from MLibSpotify import Utilities
 from MLibSpotify.SpotifyPlaylist import AuthorizationValues
 from MLibSpotify.SpotifyPlaylist import SpotifyPlaylist
 
-intents = discord.Intents.default()
-# intents.message_content = True
+intents = discord.Intents.all()
+# intents.messages = True
+# intents.guild_messages = True
 
 client = discord.Client(intents=intents)
 
@@ -17,19 +20,42 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    links = Utilities.GetSpotifyLinks(message.content)
-    if links is not None:
-        playlist_id = SelectPlaylist(message.guild)
-        authorization_values = AuthorizationValues(client_id='',
-                                                   client_secret='')
-        SpotifyPlaylist(authorization_values=authorization_values,
-                        playlist_id=playlist_id)
 
-    print(message.guild)
+    print(f'message: {message.content}')
+
+    track_uris = GetUrisFromMessage(message.content)
+
+    if track_uris is not None:
+        print('uris found:')
+        for uri in track_uris:
+            print(uri)
+        playlist = GetPlaylistByChannel(message.guild)
+        playlist.AddTracks(tracks=track_uris)
+    else:
+        print('No spotify link found in message.')
 
 
 def SelectPlaylist(guild):
     return '2UmDYQxgIDaKikeG53Ffd5'
+
+
+def GetUrisFromMessage(message):
+    track_links = Utilities.GetSpotifyLinks(message)
+    if track_links is None:
+        print('No links in message')
+        return None
+    print('links:')
+    for link in track_links:
+        print(link)
+    return [Utilities.GetUri(link) for link in track_links]
+
+
+def GetPlaylistByChannel(guild):
+    authorization_values = AuthorizationValues(client_id=GlobalSettings.CLIENT_ID,
+                                               client_secret=GlobalSettings.CLIENT_ID)
+    playlist = SpotifyPlaylist(authorization_values=authorization_values,
+                               playlist_id='2UmDYQxgIDaKikeG53Ffd5')
+    return playlist
 
 
 client.run(GlobalSettings.TOKEN)
