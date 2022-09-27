dd3b5f7a8a4899118dc75c3ef677fbed2a2b7779
unknown <mike.kennedy.dev@gmail.com>
2022-09-27 17:33:48 -0400

    Add track working

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     print(f'message: {message.content}')
 
-    track_uris = GetUrisFromMessage(message.content)
+    track_uris = GetIdsFromMessage(message.content)
 
     if track_uris is not None:
         print('uris found:')
         for uri in track_uris:
             print(uri)
         playlist = GetPlaylistByChannel(message.guild)
+        print('Trying to add tracks')
         playlist.AddTracks(tracks=track_uris)
     else:
         print('No spotify link found in message.')
@@ -39,7 +39,7 @@ def SelectPlaylist(guild):
     return '2UmDYQxgIDaKikeG53Ffd5'
 
 
-def GetUrisFromMessage(message):
+def GetIdsFromMessage(message):
     track_links = Utilities.GetSpotifyLinks(message)
     if track_links is None:
         print('No links in message')
@@ -47,15 +47,36 @@ def GetUrisFromMessage(message):
     print('links:')
     for link in track_links:
         print(link)
-    return [Utilities.GetUri(link) for link in track_links]
+    return [Utilities.GetTrackId(link) for link in track_links]
 
 
 def GetPlaylistByChannel(guild):
     authorization_values = AuthorizationValues(client_id=GlobalSettings.CLIENT_ID,
-                                               client_secret=GlobalSettings.CLIENT_ID)
+                                               client_secret=GlobalSettings.CLIENT_SECRET,
+                                               scope='playlist-read-collaborative playlist-modify-public')
+
     playlist = SpotifyPlaylist(authorization_values=authorization_values,
                                playlist_id='2UmDYQxgIDaKikeG53Ffd5')
+    print('Completed playlist modeling')
     return playlist
 
 
 client.run(GlobalSettings.TOKEN)
+
+# print('Playlist test')
+# TestPlaylistId = '2UmDYQxgIDaKikeG53Ffd5'
+# TestTrackIds = ['56rgqDNRIqKq0qIMdu7r4r', '1rWzYSHyZ5BiI4DnDRCwy7']
+# auth = AuthorizationValues(client_id=GlobalSettings.CLIENT_ID,
+#                            client_secret=GlobalSettings.CLIENT_SECRET,
+#                            scope='playlist-read-collaborative playlist-modify-public')
+# playlist = SpotifyPlaylist(auth, playlist_id=TestPlaylistId)
+#
+# original_num_tracks = len(playlist.GetAllTracks())
+# print(f'Originally {original_num_tracks} tracks')
+#
+# playlist.AddTracks(TestTrackIds)
+# print(f'{len(TestTrackIds)} tracks added')
+#
+# new_num_tracks = len(playlist.GetAllTracks(force_refresh=True))
+# print(f'Now {new_num_tracks} tracks')
+# print('Playlist test complete')
